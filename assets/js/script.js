542ddb2f523c26c593378be8a6e2c8faad907a2a
Senan <senanadigozelov555@gmail.com>
2022-10-31 16:16:35 +0400

    pizzaproject

---
+++ assets/js/script.js
@@ -0,0 +1,201 @@
+// OWL SLIDER
+$(function(){
+    const nexticon = '<i class="fa-solid fa-chevron-right"></i>'
+    const previcon = '<i class="fa-solid fa-chevron-left"></i>'
+    $('.home-slider-1').owlCarousel({
+        loop:true,
+        margin:25,
+        nav:true,
+        navText:[previcon,nexticon],
+        dots:false,
+        items:4,
+        autoplay:true,
+        autoplayTimeout:4000,
+        responsive:{
+            0:{
+                items:2,
+                nav:true,
+                loop:true,
+               
+            },
+            600:{
+                items:3,
+                nav:true,
+                loop:true,
+            },
+            1000:{
+                items:4,
+                nav:true,
+                loop:true,
+            }
+        }
+    });
+    
+    
+    });
+    
+    
+    
+    
+    $('.home-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        items:1,
+        responsiveClass:true,
+        nav:false,
+      autoplay:true,
+      autoplayTimeout:3000,
+    })
+    
+    // preloader
+    $(window).on('load',function(){
+        $('.preloader').delay(2000).fadeOut(500);
+    });
+    
+    
+    
+    
+    
+    // MENU PRODUCTS
+    $('.btnProduct').click(function(){
+        $('.btnProduct').removeClass('active');
+        $(this).addClass('active');
+    })
+    
+    $('.all').click(function(){
+        $('.iki').removeClass('app');
+        $('.bir').removeClass('app');
+        $('.uc').removeClass('app');
+        $('.dord').removeClass('app');
+        $('.bes').removeClass('app');
+        $('.alti').removeClass('app');
+        $('.yeddi').removeClass('app');
+        $('.sekkiz').removeClass('app');
+    })
+    
+    $('.drink').click(function(){
+        $('.bir').addClass('app');
+        $('.uc').addClass('app');
+        $('.dord').addClass('app');
+        $('.bes').addClass('app');
+        $('.alti').addClass('app');
+        $('.yeddi').addClass('app');
+        $('.sekkiz').addClass('app');
+        $('.iki').removeClass('app');
+    
+    })
+    
+    $('.salad').click(function(){
+        $('.bir').addClass('app');
+        $('.iki').addClass('app');
+        $('.dord').addClass('app');
+        $('.bes').addClass('app');
+        $('.alti').addClass('app');
+        $('.yeddi').addClass('app');
+        $('.uc').removeClass('app');
+        $('.sekkiz').addClass('app');
+    })
+    
+    $('.pasta').click(function(){
+        $('.bir').addClass('app');
+        $('.iki').addClass('app');
+        $('.uc').addClass('app');
+        $('.dord').removeClass('app');
+        $('.bes').addClass('app');
+        $('.alti').addClass('app');
+        $('.yeddi').addClass('app');
+        $('.sekkiz').addClass('app');
+    })
+    
+    $('.burger').click(function(){
+        $('.bir').addClass('app');
+        $('.iki').addClass('app');
+        $('.dord').removeClass('app');
+        $('.bes').addClass('app');
+        $('.alti').addClass('app');
+        $('.yeddi').removeClass('app');
+        $('.uc').removeClass('app');
+        $('.sekkiz').removeClass('app');
+    })
+    
+    $('.desert').click(function(){
+        $('.iki').removeClass('app');
+        $('.bir').addClass('app');
+        $('.uc').addClass('app');
+        $('.dord').addClass('app');
+        $('.bes').removeClass('app');
+        $('.alti').addClass('app');
+        $('.yeddi').addClass('app');
+        $('.sekkiz').addClass('app');
+    })
+    
+    $('.pizza').click(function(){
+        $('.iki').removeClass('app');
+        $('.bir').removeClass('app');
+        $('.uc').addClass('app');
+        $('.dord').removeClass('app');
+        $('.bes').addClass('app');
+        $('.alti').addClass('app');
+        $('.yeddi').addClass('app');
+        $('.sekkiz').addClass('app');
+        
+    })
+    
+    // SWIPER SLIDER
+    
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        autoplay:true,
+      autoplayTimeout:2000,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        
+      });
+    
+    
+      TweenMax.from(".img1",.5, {
+        // opacity: 0,
+        y: -420,
+        ease: Power3.ease,
+        delay:.5,
+      }, 0.02
+      )
+    
+      TweenMax.from(".img2",1, {
+        // opacity: 0,
+        y: -620,
+        ease: Power3.ease,
+        delay:.3,
+      }, 0.04
+      )
+    
+      TweenMax.from(".img3",1, {
+        // opacity: 0,
+        y: -620,
+        ease: Power3.ease,
+        delay:.4,
+      }, 0.03
+      )
+      TweenMax.from(".img4",.4, {
+        // opacity: 0,
+        y: -620,
+        ease: Power3.ease,
+        delay:.4,
+      }, 0.03
+      )
+    
+      TweenMax.from(".two",1, {
+        opacity: 0,
+        x: 320,
+        ease: Power3.ease,
+        delay:.6,
+      }, 0.03
+      )
+    // 
